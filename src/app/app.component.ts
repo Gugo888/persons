6444436d2e1a42416047a0e6b743bbeb4b446090
Gugo888 <kroyangurgen@gmail.com>
2022-01-27 21:38:02 +0300

    initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+export type Person = {
+  name: string;
+  url: string
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  persons: Person[] = [
+    {name: 'Narek', url: 'assets/narek.jpg'},
+    {name: 'Luso', url: 'assets/luso.jpg'},
+    {name: 'Zaven', url: 'assets/zaven.jpg'},
+    {name: 'Goqor', url: 'assets/goqor.jpg'},
+    {name: 'Hermine', url: 'assets/hermine.jpeg'},
+  ]
+}
---
+++ src/app/person/person.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-person',
+  templateUrl: './person.component.html',
+  styleUrls: ['./person.component.css']
+})
+export class PersonComponent implements OnInit {
+@Input() persons:any
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
